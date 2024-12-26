a537c2f4a3fed8528ae7e9cde9a39655b3a6a3e0
abdosql <abdosql@users.noreply.github.com>
2024-12-26 22:39:16 +0100

    another fix

--- init-ssl.sh
+++ init-ssl.sh
@@ -9,16 +9,23 @@ docker-compose down
 
 # Get SSL certificate
 docker run -it --rm \
-  -v "./certbot/conf:/etc/letsencrypt" \
-  -v "./certbot/www:/var/www/certbot" \
+  -v "$PWD/certbot/conf:/etc/letsencrypt" \
+  -v "$PWD/certbot/www:/var/www/certbot" \
   certbot/certbot certonly \
   --standalone \
   --preferred-challenges http \
-  --email your-email@example.com \
+  --email admin@jalalbarber.com \
   --agree-tos \
-  --no-eff-email \
+  --non-interactive \
   -d jalalbarber.com \
   -d www.jalalbarber.com
 
-# Start containers
-docker-compose up -d
+# Check if certificates were obtained successfully
+if [ $? -eq 0 ]; then
+    echo "SSL certificates obtained successfully"
+    # Start containers
+    docker-compose up -d
+else
+    echo "Failed to obtain SSL certificates. Please check the logs above for errors."
+    exit 1
+fi
